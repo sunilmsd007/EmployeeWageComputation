41402554b0cbea597c16803536523f53b3ed1f26
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-05 22:05:17 +0530

    check attendance using switch case

--- CalculateEmpWage.java
+++ CalculateEmpWage.java
@@ -2,20 +2,25 @@ package employeeWage;
 
 public class CalculateEmpWage {
 
-	public static void main(String[] args) {
-	int IS_FULL_TIME =1;
-        int IS_PART_TIME = 2;
-	int EMP_RATE_PER_HOUR = 20;
+   public static final int IS_PART_TIME = 1;
+   public static final int IS_FULL_TIME = 2;
+   public static final int EMP_RATE_PER_HOUR = 20;
+   public static void main(String[] args) {
 	int empHrs = 0;
 	int empWage = 0;
-	double empCheck = Math.floor(Math.random() * 10)% 2;
-	if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-	else if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-	else
-			empHrs = 0;
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	System.out.println("Emp Wage: "+ empWage);
-	}
+        int empCheck = (int) Math.floor(Math.random() * 10)% 3;
+	switch(empCheck) {
+	   case IS_PART_TIME:
+	        empHrs = 4;
+	        break;
+	   case IS_FULL_TIME:
+		empHrs = 8;
+	        break;
+           default:
+	        empHrs=0;
+	 }
+	 empWage = empHrs * EMP_RATE_PER_HOUR;
+	 System.out.println("Employee Wage is: " + empWage);
+ }
 }
+
