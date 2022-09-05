82a77387eb6c14070db03cc994400888e1ff3b07
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-05 22:17:06 +0530

    calculate wage till Maximum days

--- CalculateEmpWage.java
+++ CalculateEmpWage.java
@@ -2,25 +2,28 @@ package employeeWage;
 
 public class CalculateEmpWage {
 
-   public static final int IS_PART_TIME = 1;
-   public static final int IS_FULL_TIME = 2;
-   public static final int EMP_RATE_PER_HOUR = 20;
-   public static void main(String[] args) {
-	int empHrs = 0;
-	int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10)% 3;
-	switch(empCheck) {
-	   case IS_PART_TIME:
-	        empHrs = 4;
-	        break;
-	   case IS_FULL_TIME:
-		empHrs = 8;
-	        break;
-           default:
-	        empHrs=0;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static void main(String[] args) {
+	 int empWage=0, empHrs=0, totalEmployeewage=0;
+	 for(int day=0; day<NUM_OF_WORKING_DAYS; day++) {
+	      int empCheck = (int) Math.floor(Math.random() * 10)% 3;
+	      switch(empCheck) {
+	         case IS_PART_TIME:
+	           empHrs = 4;
+        	   break;
+                 case IS_FULL_TIME:
+		   empHrs = 8;
+		   break;
+		 default:
+		   empHrs=0;
+		 }
+		 empWage = empHrs * EMP_RATE_PER_HOUR;
+		 totalEmployeewage += empWage;
 	 }
-	 empWage = empHrs * EMP_RATE_PER_HOUR;
-	 System.out.println("Employee Wage is: " + empWage);
- }
+	 System.out.println("Total Employee Wage is: " + totalEmployeewage);
+     }
 }
 
