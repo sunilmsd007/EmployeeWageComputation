946e7369ec21bb8710081121014f3281c28a560f
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-05 21:51:32 +0530

    added part time employee

--- CalculateEmpWage.java
+++ CalculateEmpWage.java
@@ -4,14 +4,17 @@ public class CalculateEmpWage {
 
 	public static void main(String[] args) {
 	int IS_FULL_TIME =1;
+        int IS_PART_TIME = 2;
 	int EMP_RATE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
 	double empCheck = Math.floor(Math.random() * 10)% 2;
-	if (empCheck == IS_FULL_TIME)
-		empHrs = 8;
+	if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+	else if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
 	else
-		empHrs = 0;
+			empHrs = 0;
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	System.out.println("Emp Wage: "+ empWage);
 	}
