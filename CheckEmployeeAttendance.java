437fe2bb2db8c325a5ad5f99b9ffcdbef7ea4107
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-05 20:11:54 +0530

    code to check employee attendance

---
+++ CheckEmployeeAttendance.java
@@ -0,0 +1,13 @@
+package employeeWage;
+
+public class CheckEmployeeAttendance {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		int empCheck = (int) Math.floor(Math.random() * 10)% 2;
+		if(empCheck == IS_FULL_TIME) 
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+}
