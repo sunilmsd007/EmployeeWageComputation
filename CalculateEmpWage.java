cf044dbe0f0b59beb8a657cc6e958f4eb628d296
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-11 22:20:37 +0530

    Modified: Compute EmpWage for Multiple Companies

--- CalculateEmpWage.java
+++ CalculateEmpWage.java
@@ -1,21 +1,16 @@
 package employeeWage;
+import java.util.Scanner;
 
 public class CalculateEmpWage {
 
 	 public static final int IS_PART_TIME = 1;
 	 public static final int IS_FULL_TIME = 2;
-	 public static final int EMP_RATE_PER_HOUR = 20;
-	 public static final int NUM_OF_WORKING_DAYS = 20;
-	 public static final int MAX_HRS_IN_MONTH = 100;
-	 public static int empHrs=0;
-	 public static int empWage=0;
-	 public static int totalEmpHrs=0;
-	 public static int totalEmployeewage=0;
-	 public static int totalWorkingDays=0;
 	 
-	 public static void calculateEmpWage() {
+	 public static void calculateEmpWage(String companyName,int WagePerHr, int MaxDays, int MaxHrs) {
 	
-		 while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
+		 int empHrs=0,empWage=0,totalEmpHrs=0,totalEmployeewage=0,totalWorkingDays=0;
+
+		 while (totalEmpHrs < MaxHrs && totalWorkingDays < MaxDays ) {
 			 totalWorkingDays++;
 			 int empCheck = (int) Math.floor(Math.random() * 10)% 3;
 			 switch(empCheck) {
@@ -30,16 +25,27 @@ public class CalculateEmpWage {
 			 }
 		 
 		 totalEmpHrs += empHrs;
-		 empWage = empHrs * EMP_RATE_PER_HOUR;
+		 empWage = empHrs * WagePerHr;
 		 totalEmployeewage += empWage;
-	 }
+	    } 
+		 System.out.println("-----" +companyName+ "------");
 		 System.out.println("Total Employee WorkingDays is: " + totalWorkingDays);
 		 System.out.println("Total Employee WorkingHour is: " + totalEmpHrs);
 		 System.out.println("Total Employee Wage is: " + totalEmployeewage);
 	 }
 	 public static void main(String[] args) {
 		 
-		 calculateEmpWage();
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("enter company name");
+		 String companyName=sc.nextLine();
+		 System.out.println("enter employee wage per hour");
+		 int empWagePerHour=sc.nextInt();
+		 System.out.println("enter Maximum working days");
+		 int maxWorkingDays=sc.nextInt();
+		 System.out.println("enter maximum working hours");
+		 int maxWorkingHour=sc.nextInt();
+		 calculateEmpWage(companyName,empWagePerHour,maxWorkingDays,maxWorkingHour);
 	 }
+
 }
 
